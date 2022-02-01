d295068f694ad740edb8e8b7dd71c305ff270a75
Lautaro <lautyylazzerii@gmail.com>
2022-02-01 15:23:31 -0300

    search form JS

--- search-movie/src/App.js
+++ search-movie/src/App.js
@@ -1,17 +1,34 @@
+import React, { Component } from 'react';
 import './App.css';
 import { Title } from './components/Title';
 import { SearchForm } from './components/SearchForm';
 import 'bulma/css/bulma.css';
 
-function App() {
-  return (
+
+
+class App extends Component {
+    state = { results: [] }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
+
+  render(){
+      return (
     <div className="App">
       <Title>Search Movies with React</Title>
       <div className='SearchForm-wrapper'>
-        <SearchForm />
+        <SearchForm onResults={this._handleResults} />
       </div>
+      {this.state.results.length === 0
+        ? <p> No Results </p>
+        : <p> Con resultados </p>
+      }
     </div>
   );
+  }
+
 }
 
 export default App;
